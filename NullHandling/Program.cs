ec2b3d65c669c46a46d13104f131d124e9602a9b
KetorolNSK <fordservicenk@gmail.com>
2022-11-23 13:52:33 +0700

    Добавил символ ? после объявления типа

--- NullHandling/Program.cs
+++ NullHandling/Program.cs
@@ -2,10 +2,28 @@
 
 namespace NullHandling
 {
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
     class Program
     {
+#nullable enable
         static void Main(string[] args)
         {
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
+
+
+
+
             int thisCannotBeNull = 4;
             //thisCannotBeNull = null;
 
